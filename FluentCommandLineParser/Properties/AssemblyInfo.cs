f0f158c3d1a81c2958fba062ba50a1e2d7f412ff
WouterTinus <wouter.tinus@gmail.com>
2019-09-04 22:03:29 +0200

    Convert to .NET Standard

--- FluentCommandLineParser/Properties/AssemblyInfo.cs
+++ FluentCommandLineParser/Properties/AssemblyInfo.cs
@@ -30,11 +30,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fluent Command Line Parser")]
 [assembly: AssemblyDescription("A simple, strongly typed .NET C# command line parser library using a fluent easy to use interface.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fluent Command Line Parser")]
 [assembly: AssemblyCopyright("Copyright © Simon Williams 2012 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -50,6 +46,3 @@ using System.Runtime.InteropServices;
 // Allows us to unit test the 'internals'
 //[assembly: InternalsVisibleTo("FluentCommandLineParser.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ad2e88658ff62cac7d7ececd3ac22c6d39ee4e19a09d61acae936f9497df38fa3db020d2b607c8176cd754c7e3a8cdc10559bedcbaaeed76e277f0d009b39bab687261567a1f2da2c3d63b913822ee944664e29bcb85d6b49b87c7d6ee44647ec5252379ed5e4c09d787f6753cf2fdf4a1c1890eedc655738d466bb6f3b91396")]
 [assembly: InternalsVisibleTo("FluentCommandLineParser.Tests")]
-// !! DO NOT CHANGE - VERSIONS ARE HANDLED AUTOMATICALLY FROM THE CONTINUOUS INTEGRATION SERVER!!
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
